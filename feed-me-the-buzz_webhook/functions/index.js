6540b1a85571cc53a5289d14592ee59e2bad5faf
keerthanapareddy <skp440@nyu.edu>
2018-10-21 14:50:19 -0400

    could not do json cleaning up

--- feed-me-the-buzz_webhook/functions/index.js
+++ feed-me-the-buzz_webhook/functions/index.js
@@ -28,23 +28,35 @@ const verbsParsed = JSON.parse(verbsRaw);
 const app = dialogflow();
 
 let outputP;
-var adverbs = {};
+var adverb = {};
 var verbs = [];
 var netflixCategories = {};
-var fortuneTelling = {};
+// var fortune = {};
 var fortuneSentences =[];
 var activity = {};
 var activity_in_sentence = [];
 var moods = {};
 
 
+const fortuneFunction = () => {
+    fortuneTellingParsed.tarot_interpretations.forEach(interpretation => {
+    fortuneSentences.push(interpretation.meanings.light);
+    return fortuneSentences;
+    console.log(fortuneSentences);
+});
+
+}
+
+let fortuneWork = fortuneFunction();
 
 var grammarSource = {
-  // 'origin': '#[#number#][#plural_nouns#][#netflixCategories#][#verbsPast#][#adverb_in_sentence#]story#',
+  'origin': ['Working yayyy #fortune#'],
   // 'story': ['#number# #plural_nouns# that #adverb_in_sentence# #verbsPast# #fortuneSentences#',
   //          'Here is why #activity_in_sentence# #will# make you feel #moods#'
   //          ],
-  'origin': ['I am #number# #netflixCategories#'],
+
+  // 'story':['I am feeling #fortuneSentences#'],
+  // 'origin': ['I am #number# #netflixCategories#'],
   // 'story': ['#number# #plural_nouns# that #adverb_in_sentence# #verbsPast# #fortuneSentences#',
   //          'Here is why #activity_in_sentence# #will# make you feel #moods#'
   //          ],
@@ -52,15 +64,48 @@ var grammarSource = {
   'number':['10','14','12','3','8','6','15','5'],
   'plural_nouns':['gifs','clips','sets','pairs','sentences','articles','tricks','Youtube videos'],  //10
   'netflixCategories': netflixCategoriesParsed.categories,
-  // 'verbsPast' :verbs,
- 	'adverb_in_sentence': _.sample(adverbsParsed.adverbs)
-  // 'fortuneSentences':_.flatten(fortuneSentences),
-  // 'will':['could','would','will','can'],
-  // 'activity_in_sentence':activity_in_sentence,
-  // 'moods':moods
+  'verbsPast' :verbs,
+ 	'adverb': adverbsParsed.adverbs,
+  'fortune': fortuneWork,
+  'will':['could','would','will','can'],
+  'activity_in_sentence':activity_in_sentence,
+  'moods': moodsParsed.moods
 };
 
+
+
 const generateString = () => {
+
+
+
+    //fortune telling
+    // for(var i = 0; i<fortuneTellingParsed.tarot_interpretations.length; i++){
+    //   for(var j = 0; j<fortuneTellingParsed.tarot_interpretations[i].meanings.light.length; j++){
+    //   	fortuneSentences.push(fortuneTelling.tarot_interpretations[i].meanings.light[j]);
+    //   	 // print(fortuneTelling.tarot_interpretations[i].meanings.light[j]);
+    //     return fortuneSentences;
+    //   }
+    // }
+
+  /*
+  //verbs
+  for(var i = 0; i< verbsPast.verbs.length; i++){
+   verbs.push(verbsPast.verbs[i].past);
+    grammarSource.verbsPast = verbs;
+  }
+
+
+
+
+  //activity
+  for(var i = 0; i < activity.categories.length; i++){
+    for(var j = 0; j < activity.categories[i].examples.length; j++){
+   	 activity_in_sentence.push(activity.categories[i].examples[j]);
+    	grammarSource.activity_in_sentence = activity_in_sentence;
+    }
+  }
+  */
+
   var grammar = tracery.createGrammar(grammarSource);
   grammar.addModifiers(tracery.baseEngModifiers);
   return grammar.flatten("#origin#");
@@ -78,49 +123,16 @@ app.intent('Get article', conv => {
 
 app.intent('Get article - yes', conv => {
   let randomString = generateString();
-  conv.ask(`Here is the first ${randomString}`);
+  conv.ask(`Here is the first one! ${randomString}`);
 });
 
 app.intent('Get article - no', conv => {
   conv.close('No worries! Have a good day');
 });
 
-/*
-const generateString = () => {
-  grammarSource.adverb_in_sentence = adverbs.adverbs;
-
-  //verbs
-  for(var i = 0; i<verbsPast.verbs.length; i++){
-   verbs.push(verbsPast.verbs[i].past);
-    grammarSource.verbsPast = verbs;
-  }
-  grammarSource.netflixCategories = netflixCategories.categories;
-  grammarSource.moods = moods.moods;
 
 
-fortuneTelling.tarot_interpretations.forEach(interpretation => {
-  fortuneSentences.push(interpretation.meaning.light)
-})
 
-  //fortune telling
-  for(var i = 0; i<fortuneTelling.tarot_interpretations.length; i++){
-    for(var j = 0; j<fortuneTelling.tarot_interpretations[i].meanings.light.length; j++){
-    	fortuneSentences.push(fortuneTelling.tarot_interpretations[i].meanings.light[j]);
-    	 // print(fortuneTelling.tarot_interpretations[i].meanings.light[j]);
-      grammarSource.fortuneSentences = fortuneSentences;
-    }
-  }
-
-  //activity
-  for(var i = 0; i < activity.categories.length; i++){
-    for(var j = 0; j < activity.categories[i].examples.length; j++){
-   	 activity_in_sentence.push(activity.categories[i].examples[j]);
-    	grammarSource.activity_in_sentence = activity_in_sentence;
-    }
-  }
-}
-
-*/
 
 
 exports.feedMeTheBuzz = functions.https.onRequest(app);
